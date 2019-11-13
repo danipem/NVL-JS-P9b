5eaae8884d7ebda9236235798597d78756a5730b
danipem <56585156+danipem@users.noreply.github.com>
2019-11-13 12:28:36 +0100

    practica

---
+++ NVL-JS-P9b.js
@@ -0,0 +1,3 @@
+//gira musical
+var gira = [["Lunes","Madrid"], ["Martes","Sevilla"], ["Miércoles","Barcelona"], 
+["Jueves","Valencia"], ["Viernes","Zaragoza"], ["Sábado","Bilbao"],["Domingo","Donosti"]]
